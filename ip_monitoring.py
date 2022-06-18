d70afdd5719a4a740f7cfad38df7b1be75a70ee1
xuduojie <1016584076@qq.com>
2022-06-18 19:13:25 +0800

    add ip_monitoring.py

--- ip_monitoring.py
+++ ip_monitoring.py
@@ -1,27 +1,70 @@
+import shutil
+import time
 import requests
-import time,json
+import json
 import os
-#获取最新的ip地址信息
+
+
+# 爬取最新的ip地址信息
 def get_latest_ip():
     url = 'https://www.gstatic.com/ipranges/goog.json'
-    f2 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '.json'
     resp = requests.get(url)
+    json_data = json.loads(resp.text)
+    prefixes = json_data['prefixes']
 
-    #记录首次请求获取到的信息
-    f = open('f2.json', 'w+')
-    #f.write(resp.text['prefixes'])
-    #写入json格式
-    f.write(str(eval(resp.text)['prefixes']))
-
-#    for i in json_data:
-#        if 'ipv4Prefix' == i.keys:
-#            print('hahaha')
-
+    # 记录首次请求获取到的信息
+    f = open('file_01', 'w+')
+    for item in prefixes:
+        if 'ipv4Prefix' in item.keys():
+            f.write(item['ipv4Prefix'] + '\n')
+    f.close()
 
 
-#对比两个文件不同之处，并返回结果
+# 对比两个文件不同之处，并返回结果
+def compare_ip_list():
+    message = ''
+    if os.path.exists('file_02'):
+        fp1 = open('file_01')
+        fp2 = open('file_02')
+        file_list1 = [i for i in fp1]
+        file_list2 = [x for x in fp2]
+        fp1.close()
+        fp2.close()
+        for item in file_list1:
+            if item not in file_list2:
+                message = (message + 'Mirana WorkSpace ip 新增：' + item)
+        for item in file_list2:
+            if item not in file_list1:
+                message = (message + 'Mirana WorkSpace ip 减少：' + item)
+    else:
+        with open('file_02', 'w'):
+            message = '首次数据已获取'
+    return message
+
+
+# 发送 webhook 请求
+def webhook_request(message_content):
+    webhook_url = 'https://open.feishu.cn/open-apis/bot/v2/hook/*****'
+    headers = {'Content-Type': 'application/json'}
+    webhook_message = '【Mirana WorkSpace ip地址变更告警】 \n【详细信息】：\n %s' % message_content
+    payload = {'msg_type': 'text', 'content': {'text': webhook_message}}
+    requests.post(webhook_url, headers=headers, json=payload)
 
 
 if __name__ == '__main__':
-    get_latest_ip()
+    try:
+        get_latest_ip()
+        send_message = compare_ip_list()
+        if len(send_message) == 0:
+            print('数据一致')
+        elif send_message == '首次数据已获取':
+            webhook_request(send_message)
+        else:
+            # 备份旧数据
+            backup_file = time.strftime("%Y%m%d_%H:%M", time.localtime()) + '.log'
+            shutil.copyfile('file_02', backup_file)
 
+            webhook_request(send_message)
+        os.rename('file_01', 'file_02')
+    except:
+        print('数据检测失败！')
